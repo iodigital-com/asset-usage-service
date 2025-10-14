ec5f4b17651450c4fb8113e184ad611a85c4e156
Darlon Peeters <darlonmpeeters@gmail.com>
2025-10-14 11:06:34 +0200

    connection simple test

--- AssetUsageService/Function1.cs
+++ AssetUsageService/Function1.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Stylelabs.M.Sdk.WebClient;
+using Stylelabs.M.Sdk.WebClient.Authentication;
 
 namespace AssetUsageService;
 
@@ -17,6 +19,19 @@ public class Function1
     [Function("Function1")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
+
+        // Vervang deze waarden door jouw eigen gegevens
+        var endpoint = new Uri("stage-pasha-darlon-2.sitecoresandbox.cloud");
+
+        var oauth = new OAuthClientCredentialsGrant
+        {
+            ClientId = "AssetServiceClient",
+            ClientSecret = "E3TdCedrmWoPeqMti3L7"
+        };
+
+        var client = MClientFactory.CreateMClient(endpoint, oauth);
+        var e = client.Assets.ToString();
+
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
     }
