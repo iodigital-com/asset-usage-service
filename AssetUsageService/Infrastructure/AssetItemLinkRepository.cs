68d47d8fe28a6febc7800d4cdd2288529b8c3d3e
Darlon Peeters <darlonmpeeters@gmail.com>
2025-10-16 17:21:59 +0200

    insert een item met assets

--- AssetUsageService/Infrastructure/AssetItemLinkRepository.cs
+++ AssetUsageService/Infrastructure/AssetItemLinkRepository.cs
@@ -1,8 +1,31 @@
+using AssetUsageService.Data;
+using MongoDB.Driver;
+
 namespace AssetUsageService.Infrastructure;
 
-public class AssetItemLinkRepository
+public class AssetItemLinkRepository : IAssetItemLinkRepository
 {
-    public AssetItemLinkRepository()
+    private readonly DBContext _dbContext;
+
+    public AssetItemLinkRepository(DBContext dbContext)
     {
+        ArgumentNullException.ThrowIfNull(dbContext);
+        _dbContext = dbContext;
+    }
+
+    public async Task<AssetItemLink> InsertAssetItemLinkAsync(Guid itemId, List<int> assetIds, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(itemId);
+     
+            var newItem = new AssetItemLink
+            {
+                ItemId = itemId,
+                AssetIds = assetIds.Distinct().OrderBy(id => id).ToList(),
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+        await _dbContext.AssetItemLinks.InsertOneAsync(newItem, cancellationToken: cancellationToken);
+        return newItem;
     }
 }
--- AssetUsageService/Infrastructure/IAssetItemLinkRepository.cs
+++ AssetUsageService/Infrastructure/IAssetItemLinkRepository.cs
@@ -1,5 +1,8 @@
+using AssetUsageService.Data;
+
 namespace AssetUsageService.Infrastructure;
 
 public interface IAssetItemLinkRepository
 {
+    Task<AssetItemLink> InsertAssetItemLinkAsync(Guid itemId, List<int> assetIds, CancellationToken cancellationToken = default);
 }
