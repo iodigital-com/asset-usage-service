0e1ac7cd5d87796f6f45f5d1ab2230b2a190b09c
Pasha Ahmad <pasha.ahmad@iodigital.com>
2025-10-28 11:44:20 +0100

    working updated logs for item publish

--- iO.Sitecore.publishing/Events/PublishingEventHandler.cs
+++ iO.Sitecore.publishing/Events/PublishingEventHandler.cs
@@ -1,15 +1,19 @@
 ﻿using iO.Sitecore.Publishing.Services;
+using Sitecore.Collections;
 using Sitecore.Configuration;
 using Sitecore.Data;
 using Sitecore.Data.Events;
+using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Events;
 using Sitecore.Globalization;
 using Sitecore.Publishing;
+using Sitecore.Publishing.Pipelines.Publish;
 using Sitecore.Publishing.Pipelines.PublishItem;
 using System;
 using System.Linq;
 using Version = Sitecore.Data.Version;
+using ID = Sitecore.Data.ID;
 
 namespace iO.Sitecore.Publishing.Events
 {
@@ -21,6 +25,175 @@ namespace iO.Sitecore.Publishing.Events
         public PublishingEventHandler()
         {
             publishTelemetryService = new PublishTelemetryService(new AssetUsageServiceClient(), AuditLogPath);
+        }
+
+        public void OnPublishComplete(object sender, EventArgs eventArgs)
+        {
+            try
+            {
+                Log.Info("============ [OnPublishComplete] START ============", this);
+
+                var sitecoreEventArgs = eventArgs as SitecoreEventArgs;
+                if (sitecoreEventArgs == null)
+                {
+                    Log.Warn("[OnPublishComplete] EventArgs is not SitecoreEventArgs", this);
+                    return;
+                }
+
+                Log.Info($"[OnPublishComplete] Event Name: {sitecoreEventArgs.EventName}", this);
+
+                // Parameter[1]: Number of items processed
+                var itemsProcessedCount = sitecoreEventArgs.Parameters[1];
+                Log.Info($"[OnPublishComplete] *** ITEMS PROCESSED: {itemsProcessedCount} ***", this);
+
+                // Parameter[0]: DistributedPublishOptions
+                var param0 = sitecoreEventArgs.Parameters[0];
+                if (param0 is System.Collections.IEnumerable enumerable)
+                {
+                    foreach (var item in enumerable)
+                    {
+                        if (item == null) continue;
+
+                        var itemType = item.GetType();
+
+                        // Extract properties
+                        var targetDbNameProp = itemType.GetProperty("TargetDatabaseName");
+                        var sourceDbNameProp = itemType.GetProperty("SourceDatabaseName");
+                        var rootItemIdProp = itemType.GetProperty("RootItemId");
+                        var itemIdsToPublishProp = itemType.GetProperty("ItemIdsToPublish");
+                        var languageNameProp = itemType.GetProperty("LanguageName");
+                        var modeProp = itemType.GetProperty("Mode");
+                        var publishDateProp = itemType.GetProperty("PublishDate");
+
+                        var targetDbName = targetDbNameProp?.GetValue(item, null) as string;
+                        var sourceDbName = sourceDbNameProp?.GetValue(item, null) as string;
+                        var rootItemId = rootItemIdProp?.GetValue(item, null);
+                        var itemIdsToPublish = itemIdsToPublishProp?.GetValue(item, null);
+                        var languageName = languageNameProp?.GetValue(item, null) as string;
+                        var mode = modeProp?.GetValue(item, null);
+                        var publishDate = publishDateProp?.GetValue(item, null);
+
+                        Log.Info("========== PUBLISH DETAILS ==========", this);
+                        Log.Info($"[OnPublishComplete] Source Database: {sourceDbName}", this);
+                        Log.Info($"[OnPublishComplete] Target Database: {targetDbName}", this);
+                        Log.Info($"[OnPublishComplete] Mode: {mode}", this);
+                        Log.Info($"[OnPublishComplete] Language: {languageName}", this);
+                        Log.Info($"[OnPublishComplete] Publish Date: {publishDate}", this);
+                        Log.Info($"[OnPublishComplete] Root Item ID: {rootItemId}", this);
+
+                        // Get target database
+                        if (!string.IsNullOrEmpty(targetDbName))
+                        {
+                            var targetDb = Factory.GetDatabase(targetDbName);
+                            var sourceDb = Factory.GetDatabase(sourceDbName);
+
+                            if (targetDb != null && sourceDb != null)
+                            {
+                                Log.Info("========== UPDATED ITEMS FROM TARGET DATABASE ==========", this);
+
+                                // Get language
+                                var language = string.IsNullOrEmpty(languageName)
+                                    ? Language.Parse("en")
+                                    : Language.Parse(languageName);
+
+                                // Process ItemIdsToPublish collection
+                                if (itemIdsToPublish != null && itemIdsToPublish is System.Collections.IEnumerable itemIds)
+                                {
+                                    var processedCount = 0;
+                                    foreach (var itemIdObj in itemIds)
+                                    {
+                                        processedCount++;
+
+                                        if (itemIdObj is Guid guid)
+                                        {
+                                            var itemId = new ID(guid);
+
+                                            // Get item from TARGET database (published version)
+                                            var targetItem = targetDb.GetItem(itemId, language);
+
+                                            // Get item from SOURCE database (original version)
+                                            var sourceItem = sourceDb.GetItem(itemId, language);
+
+                                            if (targetItem != null)
+                                            {
+                                                Log.Info($"---------- UPDATED ITEM #{processedCount} ----------", this);
+                                                Log.Info($"[OnPublishComplete] *** ITEM UPDATED IN TARGET ***", this);
+                                                Log.Info($"  Path: {targetItem.Paths.FullPath}", this);
+                                                Log.Info($"  ID: {targetItem.ID}", this);
+                                                Log.Info($"  Name: {targetItem.Name}", this);
+                                                Log.Info($"  Template: {targetItem.TemplateName} ({targetItem.TemplateID})", this);
+                                                Log.Info($"  Language: {targetItem.Language.Name}", this);
+                                                Log.Info($"  Version: {targetItem.Version.Number}", this);
+                                                Log.Info($"  Updated: {targetItem.Statistics.Updated}", this);
+                                                Log.Info($"  Updated By: {targetItem.Statistics.UpdatedBy}", this);
+                                                Log.Info($"  Revision: {targetItem.Statistics.Revision}", this);
+
+                                                // Log non-empty fields
+                                                Log.Info($"  --- Non-Empty Fields ---", this);
+                                                var nonEmptyFields = targetItem.Fields
+                                                    .Where(f => !string.IsNullOrEmpty(f.Value) && !f.Name.StartsWith("__"))
+                                                    .Take(10);
+
+                                                foreach (var field in nonEmptyFields)
+                                                {
+                                                    var fieldValue = field.Value.Length > 100
+                                                        ? field.Value.Substring(0, 100) + "..."
+                                                        : field.Value;
+                                                    Log.Info($"    {field.Name}: {fieldValue}", this);
+                                                }
+
+                                                // Compare with source to see if it was actually updated
+                                                if (sourceItem != null)
+                                                {
+                                                    var sourceRevision = sourceItem.Statistics.Revision;
+                                                    var targetRevision = targetItem.Statistics.Revision;
+
+                                                    if (sourceRevision == targetRevision)
+                                                    {
+                                                        Log.Info($"  Status: ✓ UPDATED (Revisions match: {sourceRevision})", this);
+                                                    }
+                                                    else
+                                                    {
+                                                        Log.Info($"  Status: ⚠ SKIPPED (Source: {sourceRevision}, Target: {targetRevision})", this);
+                                                    }
+                                                }
+                                            }
+                                            else
+                                            {
+                                                Log.Warn($"[OnPublishComplete] Item {itemId} not found in target database", this);
+
+                                                if (sourceItem != null)
+                                                {
+                                                    Log.Info($"  Item exists in source: {sourceItem.Paths.FullPath}", this);
+                                                    Log.Info($"  Status: ⚠ SKIPPED or FAILED", this);
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    Log.Info($"[OnPublishComplete] Total Items in Collection: {processedCount}", this);
+                                }
+                                else
+                                {
+                                    Log.Warn("[OnPublishComplete] ItemIdsToPublish is null or not enumerable", this);
+                                }
+                            }
+                            else
+                            {
+                                Log.Warn($"[OnPublishComplete] Could not get databases. Target: {targetDb?.Name}, Source: {sourceDb?.Name}", this);
+                            }
+                        }
+
+                        break; // Only process first DistributedPublishOptions
+                    }
+                }
+
+                Log.Info("============ [OnPublishComplete] END ============", this);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"[OnPublishComplete] Error: {ex.Message}", ex, this);
+            }
         }
 
         public void OnItemProcessed(object sender, EventArgs eventArguments)
