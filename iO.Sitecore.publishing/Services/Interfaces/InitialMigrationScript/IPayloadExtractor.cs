cded091ac6d60ed9332cffe90fda66deaa892efd
Pasha Ahmad <pasha.ahmad@iodigital.com>
2026-01-20 12:18:36 +0100

    restructured code for single responsibiltiy

---
+++ iO.Sitecore.publishing/Services/Interfaces/InitialMigrationScript/IPayloadExtractor.cs
@@ -0,0 +1,11 @@
+﻿using iO.Sitecore.Publishing.Models;
+using Sitecore.Data.Items;
+using System.Collections.Generic;
+
+namespace iO.Sitecore.publishing.Services.Interfaces.InitialMigrationScript
+{
+    public interface IPayloadExtractor
+    {
+        List<AssetUsageEvent> ExtractPayloads(Item[] items);
+    }
+}
---
+++ iO.Sitecore.publishing/Services/Interfaces/InitialMigrationScript/IPayloadSender.cs
@@ -0,0 +1,14 @@
+﻿using iO.Sitecore.Publishing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iO.Sitecore.publishing.Services.Interfaces.InitialMigrationScript
+{
+    public interface IPayloadSender
+    {
+        Task SendPayloadsAsync(List<AssetUsageEvent> payloads);
+    }
+}
